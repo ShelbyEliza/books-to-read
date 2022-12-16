b493d194c12ddcace5426064122abba7c2f4928e
Shelby Shipley <shelbyelizashipley@gmail.com>
2022-12-16 09:59:24 -0800

    set up basis for email verification

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,37 +1,72 @@
 import { useState } from "react";
-import { useAuthContext } from "./useAuthContext";
 import { auth, db } from "../firebase/config";
+// import { useAuthContext } from "./useAuthContext";
+// import { useLogout } from "./useLogout";
 
 // firebase imports:
 import { doc, setDoc } from "firebase/firestore";
-import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signOut,
+  sendEmailVerification,
+} from "firebase/auth";
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useAuthContext();
+  // const { dispatch } = useAuthContext();
+  // const { logout: logoutHook } = useLogout();
 
-  const signup = (email, password, displayName) => {
+  const signup = async (email, password, displayName) => {
     setError(null);
     setIsPending(true);
+    const createUser = async () => {
+      console.log("1. CreatingUser");
+      return await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+        displayName
+      );
+    };
+    const updateUserProfile = async (userCredential) => {
+      console.log("2. UpdatingUser");
+      updateProfile(userCredential.user, displayName);
+    };
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((res) => {
-        updateProfile(res.user, { displayName }).then(() => {
-          let ref = doc(db, "users", res.user.uid);
-          setDoc(ref, { displayName });
-        });
-        return res;
-      })
-      .then((res) => {
-        setIsPending(false);
-        if (auth.currentUser !== null) {
-        }
-        dispatch({ type: "LOGIN", payload: res.user });
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
+    const updateUserDoc = async (userCredential) => {
+      console.log("4. UpdatingUserDoc");
+      let ref = doc(db, "users", userCredential.user.uid);
+      await setDoc(ref, displayName);
+    };
+    const sendUserEmail = async () => {
+      console.log("5. SendingUserEmail");
+      setIsPending(false);
+      await sendEmailVerification(auth.currentUser);
+      console.log("Email verification sent!");
+    };
+    const signUserOut = async () => {
+      if (auth.currentUser !== null) {
+        console.log("6. SigningUserOut");
+        console.log(auth.currentUser);
+        signOut(auth);
+      }
+    };
+
+    // await createUser()
+    // await updateUserProfile()
+    // await updateUserDoc()
+    // await sendUserEmail()
+    // await signUserOut()
+
+    let userCredential = await createUser().then(() => {
+      console.log("1. CreatingUser");
+    });
+    await updateUserProfile(userCredential);
+    await updateUserDoc(userCredential);
+    await sendUserEmail();
+    await signUserOut();
   };
 
   return { signup, error, isPending };
--- src/pages/admin/Signup.js
+++ src/pages/admin/Signup.js
@@ -1,20 +1,52 @@
 // styles:
 import styles from "./Admin.module.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSignup } from "../../hooks/useSignup";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
+  const [signupError, setSignupError] = useState(null);
+  const [signupComplete, setSignupComplete] = useState(false);
 
   const { signup, isPending, error } = useSignup();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    signup(email, password, displayName);
+    await signup(email, password, displayName);
+    setSignupComplete(true);
+    // await redirectPage(signupResponse);
+    // if (!error) {
+    //   // console.log("right before logout");
+    //   // logout();
+    //   console.log("right before redirect");
+    //   navigate("/login");
+    // } else {
+    //   setSignupError(error);
+    // }
   };
+  // const redirectPage = async (response) => {
+  //   if (!error) {
+  //     console.log(response);
+  //     navigate("/login");
+  //   } else {
+  //     setSignupError(error);
+  //   }
+  // };
+  useEffect(() => {
+    if (signupComplete === true) {
+      if (!error) {
+        console.log("Signup is complete.");
+        navigate("/login");
+      } else {
+        setSignupError(error);
+      }
+    }
+  }, [signupComplete, error, navigate]);
 
   return (
     <div className={styles.content}>
@@ -59,7 +91,7 @@ export default function Signup() {
             </button>
           )}
         </div>
-        {error && <div className="error">{error}</div>}
+        {signupError && <div className="error">{signupError}</div>}
       </form>
     </div>
   );
